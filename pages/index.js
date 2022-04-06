eb8ff036e6bf2f685a45b64b3cd14a8cd7108c2a
Khoi Nguyen <ndkhoi.dev@gmail.com>
2022-04-06 16:23:28 +0700

    display cards

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
-import Banner from "../components/Banner";
+import Banner from "../components/banner";
+import Card from "../components/card";
 import styles from "../styles/Home.module.css";
+import coffeeStores from "../data/coffee-stores.json";
 
 export default function Home() {
   const handleOnBannerBtnClick = () => {};
@@ -24,6 +26,16 @@ export default function Home() {
             height={400}
             alt="hero image"
           />
+        </div>
+        <div className={styles.cardLayout}>
+          {coffeeStores.map((store) => (
+            <Card
+              key={store.id}
+              name={store.name}
+              imgUrl={store.imgUrl}
+              href={`/coffee-store/${store.id}`}
+            />
+          ))}
         </div>
       </main>
     </div>
